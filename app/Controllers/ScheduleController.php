0b3c546f659dcde36ddeb452d6b6d7b3be506037
Dejan <dejan.tomic@yahoo.com>
2020-08-20 22:46:17 +0200

    add google client and connect to calendar api

--- app/Controllers/ScheduleController.php
+++ app/Controllers/ScheduleController.php
@@ -6,11 +6,18 @@ namespace LLT\Controllers;
 use LLT\Init\Controller;
 use LLT\Init\Validator;
 
+use Google_Client;
+use Google_Service_Calendar;
+use Google_Service_Calendar_Event;
+use Google_Service_Calendar_EventDateTime;
+
 class ScheduleController extends Controller
 {
     private $name, $phone, $email, $date;
 
     private $errorUserInput = [];
+
+    private $client;
 
     public function __construct($method, $param)
     {
@@ -19,6 +26,8 @@ class ScheduleController extends Controller
 
     public function index()
     {
+
+
         if($_SERVER['REQUEST_METHOD'] == 'POST') {
             $name = Validator::name($_POST['name']);
             if(is_array($name)) {
@@ -42,7 +51,62 @@ class ScheduleController extends Controller
 
     public function makeSchedule()
     {
-        echo "Scheduled";
+        $client = new Google_Client();
+        $client->setAuthConfig(VIEWS . 'credentials.json');
+        $client->addScope(Google_Service_Calendar::CALENDAR);
+
+        $guzzleClient = new \GuzzleHttp\Client(array('curl' => array(CURLOPT_SSL_VERIFYPEER => false)));
+        $client->setHttpClient($guzzleClient);
+        $this->client = $client;
+
+        if (isset($_SESSION['access_token']) && $_SESSION['access_token']) {
+            $this->client->setAccessToken($_SESSION['access_token']);
+            $service = new Google_Service_Calendar($this->client);
+
+            $calendarId = 'primary';
+
+            $results = $service->events->listEvents($calendarId);
+            return $results->getItems();
+
+        }else{
+            $rurl = "http://localhost:3000/schedule/makeSchedule/";
+            $this->client->setRedirectUri($rurl);
+            if (!isset($_GET['code'])) {
+                $auth_url = $this->client->createAuthUrl();
+                $filtered_url = filter_var($auth_url, FILTER_SANITIZE_URL);
+
+                header('Location: ' . $filtered_url);
+            } else {
+                $this->client->authenticate($_GET['code']);
+                $_SESSION['access_token'] = $this->client->getAccessToken();
+
+                $this->storeEvent();
+
+            }
+        }
+    }
+
+    public function storeEvent(){
+        if (isset($_SESSION['access_token']) && $_SESSION['access_token']) {
+            $this->client->setAccessToken($_SESSION['access_token']);
+            $service = new Google_Service_Calendar($this->client);
+
+            $calendarId = 'primary';
+            $event = new Google_Service_Calendar_Event([
+                'summary' => "Još jedan test bez podataka",
+                'description' => "valjda je uspelo",
+                'start' => ['date' => '2020-08-30'],
+                'end' => ['date' => '2020-09-01'],
+                'reminders' => ['useDefault' => true],
+            ]);
+            $results = $service->events->insert($calendarId, $event);
+            if (!$results) {
+                //return response()->json(['status' => 'error', 'message' => 'Something went wrong']);
+                echo "došlo je do greške";
+            }
+            //return response()->json(['status' => 'success', 'message' => 'Event Created']);
+            echo "napokon!";
+        }
     }
 
 
--- index.php
+++ index.php
@@ -1,6 +1,12 @@
 <?php
 
+ini_set('display_startup_errors', 1);
+ini_set('display_errors', 1);
+error_reporting(-1);
+
 require_once 'constants.php';
+
+require __DIR__ . '/vendor/autoload.php';
 
 spl_autoload_register(function ($class) {
 
