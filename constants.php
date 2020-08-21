16c9892881e4b9fd4d884bc2a5255bcbb6896770
Dejan <dejan.tomic@yahoo.com>
2020-08-22 01:36:09 +0200

    minor refactoring

--- constants.php
+++ constants.php
@@ -1,9 +1,4 @@
 <?php
 
 define('ROOT', $_SERVER['DOCUMENT_ROOT']);
-define('DB_NAME', '');
-define('DB_HOST', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', 'dejan');
 define('VIEWS', __DIR__ . '/views/');
-define('CSS', __DIR__.'/views/media/css');
